3211fbb714771dcd8e38a8a344f3653810f316a8
Sergey <ryabovsergey12@gmail.com>
2020-09-13 06:07:26 +0200

    implements createElement

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 const content = (
   <div>
-    <h1>Hello, React!</h1>
+    <h1 onClick={() => console.log("clicked h1")}>Hello, React!</h1>
     <ul>
       {[1, 2, 3].map((item) => (
         <li>item</li>
@@ -13,3 +13,11 @@ const content = (
     </div>
   </div>
 );
+
+function createElement(elementType, props, ...children) {
+  return {
+    elementType,
+    props,
+    children,
+  };
+}
