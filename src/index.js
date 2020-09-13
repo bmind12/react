56881fbb76259be29eb6f9dfbe83129b5845a384
Sergey <ryabovsergey12@gmail.com>
2020-09-13 05:46:45 +0200

    inits project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/** @jsxRuntime classic */
+
+const content = (
+  <div>
+    <h1>Hello, React!</h1>
+    <ul>
+      {[1, 2, 3].map((item) => (
+        <li>item</li>
+      ))}
+    </ul>
+    <div>
+      <p>Some text</p>
+    </div>
+  </div>
+);
