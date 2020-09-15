b244309acc60a1f341b637eb9fd093eeb7660503
Sergey <ryabovsergey12@gmail.com>
2020-09-15 05:02:28 +0200

    implements buildDOM function

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function createElement(elementType, props, ...children) {
   return {
     elementType,
     props,
-    children,
+    children: children.flat(),
   };
 }
 
@@ -27,3 +27,23 @@ function render(vdom, container) {
 
   container.appendChild(element);
 }
+
+function buildDOM(vnode) {
+  if (typeof vnode === "string") {
+    return document.createTextNode(vnode);
+  }
+
+  const node = document.createElement(vnode.elementType);
+
+  for (prop in vnode.props) {
+    node.setAttribute(prop, vnode.props[prop]);
+  }
+
+  for (child of vnode.children) {
+    node.appendChild(buildDOM(child));
+  }
+
+  return node;
+}
+
+render(content, document.querySelector("#app"));
